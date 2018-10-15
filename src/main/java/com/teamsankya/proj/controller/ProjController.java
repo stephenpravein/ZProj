ced6a36d54946108b76ebdd7f1efc71e14eafad2
stephenpravein <stephenpravein@outlook.com>
2018-10-15 17:51:56 +0530

    made some changes with pmd

--- src/main/java/com/teamsankya/proj/controller/ProjController.java
+++ src/main/java/com/teamsankya/proj/controller/ProjController.java
@@ -1,5 +1,16 @@
 package com.teamsankya.proj.controller;
-
+/**
+ * 
+ * @author Stephen Pravein
+ *
+ */
 public class ProjController {
-
+	/**
+	 * 
+	 * @param id
+	 * @param name
+	 */
+	public ProjController(int id,String name) {
+		// TODO Auto-generated constructor stub
+	}
 }
