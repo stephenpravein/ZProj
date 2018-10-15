bf07e950a105ef876ab09f9a57467f5564d6b3ff
stephenpravein <stephenpravein@outlook.com>
2018-10-15 15:42:32 +0530

    added fields in dto

--- src/main/java/com/teamsankya/proj/dto/ProjDTO.java
+++ src/main/java/com/teamsankya/proj/dto/ProjDTO.java
@@ -2,4 +2,41 @@ package com.teamsankya.proj.dto;
 
 public class ProjDTO {
 
+	private int id;
+	private String name;
+	private String company;
+	private String projectName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
 }
